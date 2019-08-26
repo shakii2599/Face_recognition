f0716d1bf97127a89c625ce95bd9ade60aa858a4
shakii2599 <51628077+shakii2599@users.noreply.github.com>
2019-08-26 10:56:00 +0530

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+__author__ = """shakii2599"""
+__email__ = 'sshakila2599@gmail.com'
+__version__ = '1.2.3'
+
+from .api import load_image_file, face_loc, batch_face_loc, face_landmarks, face_encodings, compare_faces, face_distance
